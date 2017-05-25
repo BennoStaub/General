d210dee220cfb6631f84ef194339df621a3c2b89
BennoStaub <staubbe@student.ethz.ch>
2017-05-25 13:35:22 +0200

    Added option to insert new expressions into library

--- LearnExpressions/voc.cpp
+++ LearnExpressions/voc.cpp
@@ -13,7 +13,8 @@
 	or because they need to be learned more because they have often been wrong in past learning sessions.
 	This results in optimal focus on expressions.
 
-	To finish the learning session, write quit as input.
+	To finish the learning session, write quit as input. As remark, it will list all wrong answered expressions so that they can 
+	looked at and learned for further progress.
 */
 
 #include <iostream>
@@ -36,105 +37,145 @@ bool cmp(sample i, sample j) { return i.value<j.value; }
 
 int main()
 {
-	// Variable declaration
-	string aray;
-	int number_lines = 0;
-	string input;
-	sample * minval;
+	cout << "Hi, what do you want? \n[L]Learn words \n[A]Add words ";
+	string action;
+	getline(cin, action);
+	if (action == "L") {
 
-	// Open file
-	ifstream infile("library.txt");
 
-	// Write data from file into array 
-	while (getline(infile, aray))
-	{
-		number_lines++;
-	}
-	sample data[number_lines];
-	infile.clear();
-	infile.seekg(0, infile.beg);
-	int iterate = 0;
-	while (getline(infile, aray))
-	{
-		string delimiter = ",";
-		string delimiter2 = ";";
-		string token = aray.substr(0, aray.find(delimiter));
-		string token2 = aray.substr(aray.find(delimiter) + 1, aray.find(delimiter2)-aray.find(delimiter)-1);
-		string value = aray.substr(aray.find(delimiter2)+1,string::npos);
-		data[iterate].it = token;
-		data[iterate].ge = token2;
-		string::size_type sz;
-		int d;
-		istringstream(value) >> d;
-		data[iterate].value = d;
-		++iterate;
-	}
 
-	// Close file
-	infile.close();
+		// Variable declaration
+		string aray;
+		int number_lines = 0;
+		string input;
+		sample * minval;
 
-	cout << endl << "Hi, nice to see you motivated to learn words!" << endl;
-	cout << "So far, there are " << number_lines << " words in your list" << endl;
+		// Open file
+		ifstream infile("library.txt");
 
-	int weaksamples[number_lines];
-	int index;
-	while (input != "quit")
-	{
-		// Define: In (percent)% of all cases, only use expressions with lowest score in library, otherwise use all expressions from library.
-		srand(time(NULL));
-		int old_new = rand() % 100;
-		int iterweak = 0;
-		int percent = 67;
-		if (old_new <= percent) 
+		// Write data from file into array 
+		while (getline(infile, aray))
 		{
-			// Create index array with only low score expressions and choose random index out of it
-			minval = min_element(data, data+number_lines, cmp);
-			for (int i = 0; i <= number_lines; i++)
-			{
-				if (data[i].value == minval->value)
-					weaksamples[iterweak++] = i;
-			}
-			int indexweak = rand() % iterweak;
-			index = weaksamples[indexweak];
+			number_lines++;
 		}
-		else {
-			// Choose random index among all expressions
-			index = rand() % number_lines;
+		sample data[number_lines];
+		infile.clear();
+		infile.seekg(0, infile.beg);
+		int iterate = 0;
+		while (getline(infile, aray))
+		{
+			string delimiter = ",";
+			string delimiter2 = ";";
+			string token = aray.substr(0, aray.find(delimiter));
+			string token2 = aray.substr(aray.find(delimiter) + 1, aray.find(delimiter2) - aray.find(delimiter) - 1);
+			string value = aray.substr(aray.find(delimiter2) + 1, string::npos);
+			data[iterate].it = token;
+			data[iterate].ge = token2;
+			string::size_type sz;
+			int d;
+			istringstream(value) >> d;
+			data[iterate].value = d;
+			++iterate;
 		}
 
-		cout << endl << data[index].ge << " = ";
-		getline(cin, input);
-		
-		if (input != "quit")
+		// Close file
+		infile.close();
+
+		cout << endl << "Hi, nice to see you motivated to learn words!" << endl;
+		cout << "So far, there are " << number_lines << " words in your list" << endl;
+
+		int weaksamples[number_lines];
+		int wronganswered[100] = {0};
+		int wrong = 0;
+		int index;
+		while (input != "quit")
 		{
-			// If input is correct, increase score of corresponding expression, otherwise show correct input and decrease score.
-			if (input == data[index].it)
+			// Define: In (percent)% of all cases, only use expressions with lowest score in library, otherwise use all expressions from library.
+			srand(time(NULL));
+			int old_new = rand() % 100;
+			int iterweak = 0;
+			int percent = 67;
+			if (old_new <= percent)
 			{
-				cout << "Correct!" << endl;
-				data[index].value++;
-			}
-			else
-			{
-				cout << "Wrong, correct would be " << data[index].it << endl;
-				if (data[index].value >= 1)
+				// Create index array with only low score expressions and choose random index out of it
+				minval = min_element(data, data + number_lines, cmp);
+				for (int i = 0; i <= number_lines; i++)
 				{
-					data[index].value--;
+					if (data[i].value == minval->value)
+						weaksamples[iterweak++] = i;
+				}
+				int indexweak = rand() % iterweak;
+				index = weaksamples[indexweak];
+			}
+			else {
+				// Choose random index among all expressions
+				index = rand() % number_lines;
+			}
+
+			cout << endl << data[index].ge << " = ";
+			getline(cin, input);
+
+			if (input != "quit")
+			{
+				// If input is correct, increase score of corresponding expression, otherwise show correct input,
+				// decrease score and add word to list of wrong answered words.
+				if (input == data[index].it)
+				{
+					cout << "Correct!" << endl;
+					data[index].value++;
+				}
+				else
+				{
+					wronganswered[wrong] = index;
+					wrong++;
+					cout << "Wrong, correct would be " << data[index].it << endl;
+					if (data[index].value >= 1)
+					{
+						data[index].value--;
+					}
 				}
 			}
-		}
-		else {
-			cout << endl <<  "Have a nice day." << endl << endl;
+			else {
+				// List all wrong answered words so that they can be learned for next sessions.
+				cout << endl << "Have a nice day. Here are the words you answered wrong:" << endl << endl;
+				for (int w=0; w <= 99; w++)
+				{
+					if (wronganswered[w] != 0)
+					{
+						cout << data[wronganswered[w]].ge << " = " << data[wronganswered[w]].it << endl;
+					}
+				}
+			}
+
 		}
 
-	}
+		// Overwrite library.txt file with improven data
+		ofstream myfile("library.txt");
+		for (int i = 0; i < number_lines; i++)
+		{
+			myfile << data[i].it << "," << data[i].ge << ";" << data[i].value << "\r\n";
+		}
+		myfile.close();
 
-	// Overwrite library.txt file with improven data
-	ofstream myfile("library.txt");
-	for (int i = 0; i < number_lines; i++)
-	{
-		myfile << data[i].it << "," << data[i].ge << ";" << data[i].value << "\r\n";
+		
 	}
-	myfile.close();
+	else {
+		ofstream myfile("library.txt", fstream::app);
+		string itw, gew;
+		while (itw != "quit") {
+			cout << "Italian word: ";
+			getline(cin, itw);
+			if (itw != "quit") {
+				cout << "German word: ";
+				getline(cin, gew);
+				myfile << itw << "," << gew << ";0\r\n";
+			}
+		}
+	
+		myfile.close();
+
+
+	}
 
 
 
